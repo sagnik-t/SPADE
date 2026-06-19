f84b95f150de895cc511f420970739ad43016d94
Sagnik Taraphdar <code.sagnik@gmail.com>
2026-06-19 13:09:52 +0530

    chore(training): export JointGenerativeTrainer

--- src/spade/training/__init__.py
+++ src/spade/training/__init__.py
@@ -9,7 +9,11 @@ Stage II. Generic parameter checkpointing for any ``nnx`` module lives in
 
 from spade.training.base import Trainer
 from spade.training.checkpoint import load_params_into, save_params
-from spade.training.generative import GenerativeTrainer, load_generative_model
+from spade.training.generative import (
+    GenerativeTrainer,
+    JointGenerativeTrainer,
+    load_generative_model,
+)
 from spade.training.representation import (
     RepresentationTrainer,
     load_representation_model,
@@ -19,6 +23,7 @@ __all__ = [
     "Trainer",
     "RepresentationTrainer",
     "GenerativeTrainer",
+    "JointGenerativeTrainer",
     "load_generative_model",
     "load_representation_model",
     "save_params",
